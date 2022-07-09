e257880078c299d9ea3d59cab2e1d9fcb8154203
Carlos Rinaldis <55250300+carlosrinaldis@users.noreply.github.com>
2022-07-08 23:02:35 -0300

    Code Images Text to appear on Click

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,6 @@
 const dropDown_block = document.querySelector(".c-header.-style.--01.--alteo .ct-content.-items.--dropdown");
 const dropDown_icon = document.querySelector(".c-header.-style.--01.--alteo .nav.-item.--09");
 const header = document.querySelector(".c-header.-style.--01.--alteo");
-let N = 0;
 
 // function firstTest() {
 //     N = N + 1;
@@ -23,4 +22,25 @@ function dropDownMenuToggle() {
 dropDown_icon.addEventListener("click", dropDownMenuToggle);
 
 console.log(header);
+
+// Image Toggle
+
+const imageGrid_block = document.querySelector(".image-grid");
+const imageGrid_imageCards = imageGrid_block.querySelectorAll (".c-image-card .ct-content.-img");
+
+function imageDetailsToggle() {
+    let n = this.querySelector(".-block.--01.-text");
+    console.log(n);
+    n.classList.toggle('monkey');
+};
+
+imageGrid_imageCards.forEach(element => {
+    element.addEventListener("click", imageDetailsToggle);
+});
+
+
+
+console.log(imageGrid_imageCards);
+
+// console.log(imageCard_textBlocks);
 
